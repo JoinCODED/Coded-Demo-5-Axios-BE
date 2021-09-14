c7f6fade7f7f434d94883ca328f323a23d1f2628
yanalshmilan <yshmilan@gmail.com>
2021-09-14 21:15:03 +0300

    done

---
+++ data.js
@@ -0,0 +1,19 @@
+const data = [
+  {
+    id: 1,
+    title: 'Wash dishes',
+    priority: 'High',
+  },
+  {
+    id: 2,
+    title: 'Go to the gym',
+    priority: 'Low',
+  },
+  {
+    id: 3,
+    title: 'Learn coding',
+    priority: 'Moderate',
+  },
+];
+
+module.exports = data;
